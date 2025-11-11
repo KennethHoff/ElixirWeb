59d92af4b4277bbe55753e062a0ce83b77897192
Kenneth Hoff <kenneth.98@hotmail.com>
2025-11-11 18:19:17 +0100

    Initialize Nix Flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.elixir
+          ];
+        };
+      }
+    );
+}
